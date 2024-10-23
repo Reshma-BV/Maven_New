8b4fe7baeeb8aa092307725531f4f16882896d46
Reshmabv <Reshmabv@Reshma.home>
2024-10-23 18:38:13 +0530

    Commit for Maven_New

---
+++ Maven_New/src/main/java/Package_new/Class_New.java
@@ -0,0 +1,17 @@
+package Package_new;
+
+public class Class_New 
+{
+
+	public static void main(String[] args) 
+	{
+		String s= "  welcome  ";
+		String s1="Selenium";
+		String s2="Java";
+		System.out.println(s1.concat(s2));
+		s.length();
+		System.out.println(s);
+		System.out.println(s.trim().length());
+	}
+
+}
